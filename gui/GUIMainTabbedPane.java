292cdb11efe0b8f1210352b7cb885c5f25290548
Russell Mull <rmull@carthage.edu>
2011-12-06 14:44:50 -0600

    gui stuff changed, for Nate to work on recommendation algorithm

--- gui/GUIMainTabbedPane.java
+++ gui/GUIMainTabbedPane.java
@@ -1,25 +1,28 @@
 package gui;
 
+import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 
+import client.ClientUser;
+
 public class GUIMainTabbedPane extends JFrame
 {
+	private static ClientUser user;
+	
 	public GUIMainTabbedPane()
 	{
 		JTabbedPane jtp = new JTabbedPane();
 		
-		jtp.add("My Profile", new GUIMyProfile());
-		jtp.add("My Consumables", new GUIMyConsumables());
-		jtp.add("My Attributes", new GUIMyAttributes());
-		jtp.add("Search", new GUISearch());
-		jtp.add("Recommendations", new GUIRecommendations());
+		jtp.add("My Profile", new GUIMyProfile(user));
+		jtp.add("My Consumables", new GUIMyConsumables(user));
+		jtp.add("My Attributes", new GUIMyAttributes(user));
+		jtp.add("Search", new GUISearch(user));
+		jtp.add("Recommendations", new GUIRecommendations(user));
 		
 		add(jtp);
-		setSize(1000, 500);
+		setSize(450, 350);
+		setMaximumSize(new Dimension(450,350));
 		setVisible(true);
-		
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
 }
